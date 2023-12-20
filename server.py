abcf983d8dca59abc7486fb69c9fea4ebe445dc7
AliceAlandi <a.alandi@itsrizzoli.it>
2023-12-20 17:10:56 +0100

    funzione filtro concerti

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import query_file as qf
+from query_file import filtra_concerti
 
 app = Flask(__name__)
 generi = qf.trova_generi()
@@ -10,18 +11,30 @@ def principale():
     results = qf.ottieni_sample_concerti()
     return render_template("principale.html", concerti=results, genres=generi)
 
-
 @app.route("/filtri")
 def concerti_filtrati():
-    ...
+    lista_generi = request.args.getlist('genere')
+    data = request.args.get('data')
+    citta = request.args.get('citta')
+    artisti = request.args.get('artisti')
+    prezzo = request.args.get('prezzo')
 
+    risultati = filtra_concerti(
+        lista_generi=lista_generi,
+        date=data,
+        citta=citta,
+        artisti=artisti,
+        prezzo=prezzo
+    )
 
-@app.route("/acquista_biglietti")
+    return render_template("principale.html", concerti=risultati)
+
+app.route("/acquista_biglietti")
 def acquisto_biglietto():
     id = request.args.get('id')
     out = qf.acquista_concerto(id_biglietto=id)
     if out:
-        return render_template('acquisto_avvenuto.html', nome=out)
+        return render_template('acquisto_avvenuto.html', nome=out['nome'])
     return render_template('errore.html', nome=out)
 
 
